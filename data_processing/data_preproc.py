eb579100ddce00c42dd89f4afc8aaa1cebcc921b
aeijpe <aniek.eijpe1@gmail.com>
2023-11-29 19:52:40 +0100

    Data preprocessing setup

--- data_processing/data_preproc.py
+++ data_processing/data_preproc.py
@@ -11,6 +11,9 @@ from monai.transforms import (
     Resized,
     ScaleIntensityd,
     SqueezeDimd,
+    MapLabelValued,
+    Flipd,
+    Rotated,
 )
 
 import matplotlib.pyplot as plt
@@ -18,25 +21,83 @@ import glob
 import torch
 import argparse
 import sys
+import SimpleITK as sitk
+import random
+
+def create_ct_data(ds, output_dir_image, output_dir_label):   
+    # Create patches (i.e. images)
+    patch_func = monai.data.PatchIterd(
+        keys=["img", "seg"],
+        patch_size=(None, None, 1),  # dynamic first two dimensions
+        start_pos=(0, 0, 0)
+    )
+
+    patch_transform = Compose(
+        [
+            SqueezeDimd(keys=["img", "seg"], dim=-1),  # squeeze the last dim
+            #Rotated(keys=["img", "seg"], angle=math.radians(180)),
+            Flipd(keys=["img", "seg"], spatial_axis=1),
+            MapLabelValued(keys=["seg"], orig_labels=[205, 420, 500, 550, 600, 820, 850], target_labels=[1, 2, 3, 4, 5 , 6 , 7]),
+        ]
+    )
+    
+
+    example_patch_ds = GridPatchDataset(data=ds, patch_iter=patch_func, transform=patch_transform)
+    patch_data_loader = DataLoader(example_patch_ds, batch_size=1, num_workers=2, pin_memory=torch.cuda.is_available())
+
+    i = 0
+    for batch in patch_data_loader:
+        image, label = batch[0]["img"], batch[0]["seg"]
+        if torch.any(label.data != 0).item():
+            # Convert the torch tensor to a SimpleITK image
+
+            slice_image = sitk.GetImageFromArray(image.squeeze(0))
+            slice_label = sitk.GetImageFromArray(label.squeeze(0))
+            # # Save the 2D slice as a NIfTI file
+            sitk.WriteImage(slice_image, os.path.join(output_dir_image, f"slice_{i}.nii.gz"))
+            sitk.WriteImage(slice_label, os.path.join(output_dir_label, f"slice_{i}.nii.gz"))
+            i += 1
+
+def create_mr_data(ds, output_dir_image, output_dir_label):
+    # Create patches (i.e. images)
+    patch_func = monai.data.PatchIterd(
+        keys=["img", "seg"],
+        patch_size=(None, 1, None),  # dynamic first two dimensions
+        start_pos=(0, 0, 0)
+    )
+
+    patch_transform = Compose(
+        [
+            SqueezeDimd(keys=["img", "seg"], dim=0),  # squeeze the last dims
+            MapLabelValued(keys=["seg"], orig_labels=[205, 420, 500, 550, 600, 820, 850], target_labels=[1, 2, 3, 4, 5 , 6 , 7]),
+        ]
+    )
+    
+
+    example_patch_ds = GridPatchDataset(data=ds, patch_iter=patch_func, transform=patch_transform)
+    patch_data_loader = DataLoader(example_patch_ds, batch_size=1, num_workers=2, pin_memory=torch.cuda.is_available())
+
+    i = 0
+    for batch in patch_data_loader:
+        image, label = batch[0]["img"], batch[0]["seg"]
+        if torch.any(label.data != 0).item():
+            # Convert the torch tensor to a SimpleITK image
+
+            slice_image = sitk.GetImageFromArray(image.squeeze(2))
+            slice_label = sitk.GetImageFromArray(label.squeeze(2))
+            # # Save the 2D slice as a NIfTI file
+            sitk.WriteImage(slice_image, os.path.join(output_dir_image, f"slice_{i}.nii.gz"))
+            sitk.WriteImage(slice_label, os.path.join(output_dir_label, f"slice_{i}.nii.gz"))
+            i += 1
 
 
 
-def create_data(data_dir, modality, output_folder):
-    if modality == "CT":
-        data_dir = os.path.join(data_dir, "ct_train")
-        output_folder_train = os.path.join(output_folder, "ct_train")
-        output_folder_val = os.path.join(output_folder, "ct_val")
-        output_folder_test = os.path.join(output_folder, "ct_test")
+def create_data_helper(modality,images, labels, output_dir):
+    output_dir_image = os.path.join(output_dir, "images")
+    output_dir_label = os.path.join(output_dir, "labels")
+    os.makedirs(output_dir_image, exist_ok=True)
+    os.makedirs(output_dir_label, exist_ok=True)    
 
-    elif modality == "MRI":
-        data_dir = os.path.join(data_dir, "mr_train")
-        output_folder_train = os.path.join(output_folder, "mr_train")
-        output_folder_val = os.path.join(output_folder, "mr_val")
-        output_folder_test = os.path.join(output_folder, "mr_val")
-
-    # Get al the files
-    images = sorted(glob.glob(os.path.join(data_dir, "ct_train_?_image.nii.gz")))
-    labels = sorted(glob.glob(os.path.join(data_dir, "ct_train_?_label.nii.gz")))
     dataset_files = [{"img": img, "seg": seg} for img, seg in zip(images, labels)]  
 
     volume_transforms = Compose(
@@ -49,26 +110,43 @@ def create_data(data_dir, modality, output_folder):
     
     # TO DO: Experiment with last two parameters
     volume_ds = CacheDataset(data=dataset_files, transform=volume_transforms, cache_rate=1.0, num_workers=4)
+
+    if modality == "CT":
+        create_ct_data(volume_ds, output_dir_image, output_dir_label)
+    elif modality == "MRI":
+        create_mr_data(volume_ds, output_dir_image, output_dir_label)
+
     
-    # Create patches (i.e. images)
-    patch_func = monai.data.PatchIterd(
-        keys=["img", "seg"],
-        patch_size=(None, None, 1),  # dynamic first two dimensions
-        start_pos=(0, 0, 0)
-    )
 
-    patch_transform = Compose(
-        [
-            SqueezeDimd(keys=["img", "seg"], dim=-1),  # squeeze the last dim
-            Resized(keys=["img", "seg"], spatial_size=[224, 224]), # resize the spatial size
-        ]
-    )
+def create_data(data_dir, modality, output_folder):
+    # get directories
+    if modality == "MRI":
+        data_dir = os.path.join(data_dir, "mr_train")
+        images = sorted(glob.glob(os.path.join(data_dir, "mr_train_*_image.nii.gz"))) 
+        labels = sorted(glob.glob(os.path.join(data_dir, "mr_train_*_label.nii.gz")))
+    elif modality == "CT":
+        data_dir = os.path.join(data_dir, "ct_train")
+        images = sorted(glob.glob(os.path.join(data_dir, "ct_train_*_image.nii.gz"))) 
+        labels = sorted(glob.glob(os.path.join(data_dir, "ct_train_*_label.nii.gz")))
+    else:
+        print("Modality not supported")
+        return
+    
+    # random shuffle the list?
+    random.shuffle(images)  
+    random.shuffle(labels)
+    
+    train_images = images[:16]
+    train_labels = labels[:16]
+    val_images = images[16:18]
+    val_labels = labels[16:18]
+    test_images = images[18:]
+    test_labels = labels[18:]
 
-example_patch_ds = GridPatchDataset(data=volume_ds_person1, patch_iter=patch_func, transform=patch_transform)
-
-
-
-
+    create_data_helper(modality, train_images, train_labels, os.path.join(output_folder, "train"))
+    create_data_helper(modality, val_images, val_labels, os.path.join(output_folder, "val"))
+    create_data_helper(modality, test_images, test_labels, os.path.join(output_folder, "test"))
+            
 
 
 if __name__ == '__main__':
@@ -80,13 +158,10 @@ if __name__ == '__main__':
     # CT or MRI
     parser.add_argument('--modality', type=str, default="CT",
                         help='name of the scene') 
-    parser.add_argument('--output_folder', type=str, default="processed/",
+    parser.add_argument('--output_folder', type=str, default="preprocessed/",
                         help='Path to the store directory.')
 
     argv = sys.argv[sys.argv.index("--") + 1:]
     args = parser.parse_args(argv)
 
     create_data(args.data_dir, args.modality, args.output_folder)
-
-
-       
